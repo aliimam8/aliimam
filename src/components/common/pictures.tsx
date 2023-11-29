4b8290ea65ce0f4fbb69ff63c6f754c213d0efb3
Ali Imam <imam30114@gmail.com>
2023-11-29 20:08:43 +0530

    jnl

--- src/components/common/pictures.tsx
+++ src/components/common/pictures.tsx
@@ -2,23 +2,17 @@
 
 import CldImage from 'src/components/CldImage';
 import { AIImage } from 'src/utils/types';
-import { PhotoProvider, PhotoView } from 'react-photo-view';
-import 'react-photo-view/dist/react-photo-view.css';
 
 export default function Favo({ src, alt }: { src: AIImage[]; alt: AIImage[] }) {
   return (
     <div className="w-full">
-      <PhotoProvider>
-        <PhotoView src={src}>
-          <CldImage
-            className="block h-full w-full cursor-zoom-in rounded-lg object-cover object-center saturate-100 transition-all duration-100 hover:saturate-0"
-            width={300}
-            height={300}
-            src={src}
-            alt={alt}
-          />
-        </PhotoView>
-      </PhotoProvider>
+      <CldImage
+        className="block h-full w-full cursor-zoom-in rounded-lg object-cover object-center saturate-100 transition-all duration-100 hover:saturate-0"
+        width={300}
+        height={300}
+        src={src}
+        alt={alt}
+      />
     </div>
   );
 }
