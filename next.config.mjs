c1c672da3e8d229e87f315844bc3d7872ecb9cbe
Ali Imam <imam30114@gmail.com>
2024-01-10 19:46:03 +0530

    kj jk

---
+++ next.config.mjs
@@ -0,0 +1,92 @@
+import { withContentlayer } from 'next-contentlayer';
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: !!process.env.CI
+  },
+
+  eslint: {
+    ignoreDuringBuilds: !!process.env.CI
+  },
+  images: {
+    domains: ['res.cloudinary.com', 'khhamnquzieyqedqyvfw.supabase.co', 'images.remotePatterns'],
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+        pathname: '/s/files/**'
+      }
+    ]
+  },
+  webpack: (config) => {
+    config.infrastructureLogging = {
+      level: 'error'
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return config;
+  },
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async redirects() {
+    return [
+      {
+        source: '/pc-specs',
+        destination: '/uses',
+        permanent: true
+      }
+    ];
+  },
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          }
+        ]
+      }
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/password',
+        destination: '/',
+        permanent: true
+      }
+    ];
+  }
+};
+
+export default withContentlayer(nextConfig);
