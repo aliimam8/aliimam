167c9f59eb00503d71417f45d5df413ec4bd0736
Ali Imam <83060154+aliimam-in@users.noreply.github.com>
2024-04-03 10:32:25 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: ["@prisma/client"],
+    concurrentFeatures: true
   },
   images: {
     domains: ['res.cloudinary.com', 'khhamnquzieyqedqyvfw.supabase.co', 'images.remotePatterns'],
